394829af4c584e14f06f002c057e22c426c3c25b
Thibault LEPEZ <thibault.lepez@efrei.net>
2022-03-30 11:46:55 +0200

    add dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -13,21 +14,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-
 const POLYGON_PRIVATE_KEY = process.env.POLYGON_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
   paths: {
-    artifacts : './src/artifacts',
+    artifacts: './src/artifacts',
   },
   networks: {
     polygon: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/1vDEUZnf1mSeJll62xCcOj8B24Wq2Mij`,
-      accounts: [`${POLYGON_PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`${POLYGON_PRIVATE_KEY}`],
+    },
+  },
 };
